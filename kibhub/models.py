8228896fb9b6343965b9feac032d971b8940ba01
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-12-29 10:03:33 +0300

    add property model

--- kibhub/models.py
+++ kibhub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+  CATEGORIES = (
+    ('house','house'),
+    ('office','office'),
+    ('shop','shop'),
+    ('kibanda','kibanda'),
+  )
+  description = models.TextField()
+  location = models.CharField(max_length=255)
+  rooms = models.IntegerField()
+  rent = models.IntegerField()
+  category = models.CharField(choices=CATEGORIES, default=CATEGORIES[0], max_length=200)
+  facilities = models.TextField()
+  more_info = models.TextField()
+
+  class Meta:
+    ordering = ['description']
