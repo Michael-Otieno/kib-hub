38189b229c725fb72db304a9d62d14ad983f1f49
Michael-Otieno <michael.otieno@student.moringaschool.com>
2023-01-06 12:11:39 +0300

    add allowed hosts

--- configurations/settings.py
+++ configurations/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-8df5.up.railway.app','127.0.0.1']
 
 
 # Application definition
